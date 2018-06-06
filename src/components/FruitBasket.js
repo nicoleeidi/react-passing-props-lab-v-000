970e51971f46145293a0dec4e5261f8603ccde8f
Nicole Eidi <nicoleeidi1@gmail.com>
2018-06-06 22:13:15 +0000

    automatically backed up by learn

--- src/components/FruitBasket.js
+++ src/components/FruitBasket.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Filter from './Filter';
 import FilteredFruitList from './FilteredFruitList.js';
 
-const FruitBasket = ({ fruit, filters, currentFilter, onUpdateFilter }) => 
+const FruitBasket = ({ fruit, filters, currentFilter, onUpdateFilter }) =>
   <div className="fruit-basket">
     <Filter
       filters={filters}
